04b74459758ada045364c6a1ca577e9240b744a4
Travis Collins <erbrecht@gmail.com>
2023-02-13 11:57:12 -0500

    manual config

--- src/main/java/com/example/eureka/SecurityConfig.java
+++ src/main/java/com/example/eureka/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.example.eureka;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +19,31 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().ignoringRequestMatchers("/eureka/**");
+        http.csrf().ignoringRequestMatchers("/eureka/**")
+                .and().securityMatcher("/**").authorizeHttpRequests().anyRequest().authenticated()
+                .and()
+                .formLogin().disable()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(
+                User.builder()
+                        .username("user")
+                        .password("password")
+                        .authorities("USER")
+                        .build()
+        );
+        return new InMemoryUserDetailsManager(users);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
