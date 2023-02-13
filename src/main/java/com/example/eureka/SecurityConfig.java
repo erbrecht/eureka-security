917c06cc9ff9d5daf7434144a46f7bf590328b21
Travis Collins <tec25@drexel.edu>
2023-02-13 10:34:48 -0500

    initial commit

---
+++ src/main/java/com/example/eureka/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.eureka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().ignoringRequestMatchers("/eureka/**");
+        return http.build();
+    }
+}
